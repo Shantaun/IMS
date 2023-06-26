7a3971d467015b90198dbee5a5616d283e7223c8
Shantanu Shinde <98450755+Shantaun@users.noreply.github.com>
2023-06-26 22:41:00 +0530

    Delete ims/admin directory

--- ims/admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-  <div class="az-footer">
-        <div class="container-fluid">
-          <span>&copy; 2023 Insurance Management System (IMS)</span>
-         
-        </div><!-- container -->
-      </div>
